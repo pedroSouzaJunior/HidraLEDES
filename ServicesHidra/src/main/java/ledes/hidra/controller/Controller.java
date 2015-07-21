dd0f9f34fbf1479937ee6ee2a4708cfaa7e4994c
Pedro <pedro.souza.psj@gmail.com>
2015-07-21 19:28:57 -0400

    envio de arquivos do tcc para versionamento

--- ServicesHidra/src/main/java/ledes/hidra/controller/Controller.java
+++ ServicesHidra/src/main/java/ledes/hidra/controller/Controller.java
@@ -5,12 +5,21 @@
  */
 package ledes.hidra.controller;
 
+import com.sun.jersey.multipart.FormDataMultiPart;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import ledes.hidra.services.HidraServices;
 import org.primefaces.model.UploadedFile;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
 import org.primefaces.event.FileUploadEvent;
 
 /**
@@ -24,6 +33,7 @@ public class Controller implements Serializable {
 
     private String path;
     private UploadedFile file;
+    private FormDataMultiPart form;
 
     public Controller() {
     }
@@ -38,7 +48,35 @@ public class Controller implements Serializable {
     public void upload() {
         if (file != null) {
             FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
+            //FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+
+    }
+
+    public void fileUploadAction(FileUploadEvent event) {
+        try {
+            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+            HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+
+            FacesContext aFacesContext = FacesContext.getCurrentInstance();
+            ServletContext context = (ServletContext) aFacesContext.getExternalContext().getContext();
+
+            String realPath = context.getRealPath("/");
+
+            // Aqui cria o diretorio caso não exista
+            File f = new File(realPath + "/fotos/");
+            f.mkdirs();
+
+            byte[] arquivo = event.getFile().getContents();
+            String caminho = realPath + "/fotos/" + event.getFile().getFileName();
+
+            // esse trecho grava o arquivo no diretório
+            FileOutputStream fos = new FileOutputStream(caminho);
+            fos.write(arquivo);
+            fos.close();
+
+        } catch (Exception ex) {
+            System.out.println("Erro no upload de imagem" + ex);
         }
     }
 
@@ -56,6 +94,14 @@ public class Controller implements Serializable {
 
     public void setFile(UploadedFile file) {
         this.file = file;
+    }
+
+    public FormDataMultiPart getForm() {
+        return form;
+    }
+
+    public void setForm(FormDataMultiPart form) {
+        this.form = form;
     }
 
 }
--- ServicesHidra/src/main/java/ledes/hidra/services/HidraServices.java
+++ ServicesHidra/src/main/java/ledes/hidra/services/HidraServices.java
@@ -5,16 +5,29 @@
  */
 package ledes.hidra.services;
 
+import com.sun.jersey.core.header.ContentDisposition;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataBodyPart;
+import com.sun.jersey.multipart.FormDataMultiPart;
+import com.sun.jersey.multipart.FormDataParam;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBException;
 import ledes.hidra.Hidra;
+import org.primefaces.model.UploadedFile;
 
 /**
  *
@@ -60,6 +73,57 @@ public class HidraServices {
         return "fail";
     }
 
+    /*
+     @POST
+     @Path("/upload")
+     @Consumes(MediaType.MULTIPART_FORM_DATA)
+     @Produces(MediaType.TEXT_PLAIN)
+     public String uploadFile(@FormDataParam("file") InputStream fis,
+     @FormDataParam("file") FormDataContentDisposition fdcd) {
+  
+     String FOLDER_PATH = "/home/pedro/";
+     OutputStream outpuStream = null;
+     String fileName = fdcd.getFileName();
+     System.out.println("File Name: " + fdcd.getFileName());
+     String filePath = FOLDER_PATH + fileName;
+         
+     try {
+     int read = 0;
+     byte[] bytes = new byte[1024];
+     outpuStream = new FileOutputStream(new File(filePath));
+     while ((read = fis.read(bytes)) != -1) {
+     outpuStream.write(bytes, 0, read);
+     }
+     outpuStream.flush();
+     outpuStream.close();
+     } catch(IOException iox){
+     iox.printStackTrace();
+     } finally {
+     if(outpuStream != null){
+     try{outpuStream.close();} catch(Exception ex){}
+     }
+     }
+     return "File Upload Successfully !!";
+     }
+
+     */
+    /*
+     @POST
+     @Path("/upload")
+     @Consumes(MediaType.MULTIPART_FORM_DATA)
+     public Response upload(FormDataMultiPart form, UploadedFile file, String path) {
+
+     String serverLocation = path + File.pathSeparator + file.getFileName();
+
+     FormDataBodyPart filePart = form.getField("file");
+     //ContentDisposition headerOfFilePart = filePart.getContentDisposition();
+     InputStream fileInputStream = filePart.getValueAs(InputStream.class);
+     saveFile(fileInputStream, serverLocation);
+
+     String output = "File saved to server location using FormDataMultiPart : " + serverLocation;
+     return Response.status(200).entity(output).build();
+     }
+     */
     @GET
     @Path("/add/{arg1}")
     @Produces(MediaType.TEXT_PLAIN)
@@ -85,6 +149,27 @@ public class HidraServices {
             Logger.getLogger(Hidra.class.getName()).log(Level.SEVERE, null, exception);
         }
         return "fail";
+    }
+
+    private void saveFile(InputStream fileInputStream, String serverLocation) {
+        int read = 0;
+
+        try {
+            OutputStream outputStream = new FileOutputStream(new File(serverLocation));
+
+            byte[] bytes = new byte[1024];
+
+            while ((read = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+
+            outputStream.flush();
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (IOException e) {
+            Logger.getLogger(HidraServices.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 
 }
